59ac008071edfaaef090750eb09c126aa2ed0fc6
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2023-12-14 11:34:28 -0600

    added-resume

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,10 +1,37 @@
 import React from 'react';
-import './About.css'
+import Info from '../../components/info'
+import CV from './BarbaroReyesResume.pdf'
+import {FaDownload } from 'react-icons/fa'
+import './About.css';
+
 const About = () => {
   return (
-    <div>
-      About
-    </div>
+    <main className="section container">
+     <section className="about">
+      <h2 className="section_title">
+        About <span>Me</span>
+      </h2>
+      <div className="about-info">
+        <h3 className="section_subtitle">
+          Personal Infos
+        </h3>
+        <ul className="info_list grid">
+        <Info/>
+        </ul>
+        <a href={CV} 
+        download=''
+        className="button">
+          Dowload CV
+        <span className="button_icon">
+          <FaDownload/>
+          </span>
+          </a>
+      </div>
+       <div className="stats grid">
+        
+       </div>
+     </section>
+    </main>
   );
 }
 
